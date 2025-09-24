08d73ce75d2033984e30b40fc264302d9ad0be69
OtisPresley <owenja6@gmail.com>
2025-09-24 14:26:13 -0400

    Update conftest.py
    Change

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,9 @@
 # tests/conftest.py
 import pytest
-pytest_plugins = "pytest_homeassistant_custom_component"
 
+pytest_plugins = "pytest_homeassistant_custom_component"
 
 
 @pytest.fixture(autouse=True)
 def auto_enable_custom_integrations(enable_custom_integrations):
     yield
-
