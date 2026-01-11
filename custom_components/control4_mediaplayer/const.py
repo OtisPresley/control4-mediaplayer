13b5f01430f70989571db951ea87db34f44bef83
OtisPresley <owenja6@gmail.com>
2026-01-11 11:51:43 -0500

    Update const.py

--- custom_components/control4_mediaplayer/const.py
+++ custom_components/control4_mediaplayer/const.py
@@ -6,6 +6,12 @@ CONF_PORT = "port"
 CONF_CHANNEL = "channel"
 CONF_SOURCE_LIST = "source_list"
 
+CONF_POLL_EXTERNAL = "poll_external"
+CONF_POLL_INTERVAL = "poll_interval"
+
 DEFAULT_PORT = 8750
 DEFAULT_VOLUME = 5                 # percent
 DEFAULT_SOURCE_LIST = ["1", "2", "3", "4"]
+
+DEFAULT_POLL_EXTERNAL = False
+DEFAULT_POLL_INTERVAL = 5  # seconds
